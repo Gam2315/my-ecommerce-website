53e2cbb8af97ecf8ad47d81a1d9b4056a437f058
Gam2315 <gamalielquintos725@gmail.com>
2026-07-30 16:40:29 +0800

    FIX BUGS

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,21 +1,16 @@
 "use client";
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { CartProvider } from "@/context/CartContext";
 import { ThemeProvider } from "next-themes";
 import CartSidebar from "./CartSidebar";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "placeholder-client-id";
-
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <GoogleOAuthProvider clientId={clientId}>
-        <CartProvider>
-          {children}
-          <CartSidebar />
-        </CartProvider>
-      </GoogleOAuthProvider>
+      <CartProvider>
+        {children}
+        <CartSidebar />
+      </CartProvider>
     </ThemeProvider>
   );
 }
